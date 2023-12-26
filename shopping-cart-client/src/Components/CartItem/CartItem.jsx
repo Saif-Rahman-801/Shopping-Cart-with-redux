9bea548299f6767714511d543e40e2d26d3b0d32
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-26 20:49:14 +0600

    cart item row created

--- shopping-cart-client/src/Components/CartItem/CartItem.jsx
+++ shopping-cart-client/src/Components/CartItem/CartItem.jsx
@@ -1,11 +1,42 @@
 /* eslint-disable react/prop-types */
 
-const CartItem = ({item}) => {
-    return (
-        <div>
-            <h2>{item.length} </h2>
+// import { useDispatch } from "react-redux";
+
+
+const CartItem = ({ item }) => {
+    // const dispatch = useDispatch()
+  return (
+    <tr>
+      <td className="border px-4 py-2">{item.productName} </td>
+      <td className="border px-4 py-2">${item.price.toFixed(2)} u</td>
+      <td className="border px-4 py-2">
+        <div className="flex items-center">
+          <button className="bg-blue-500 text-white px-2 py-1" onClick={() => {}}>
+            -
+          </button>
+          <input
+            type="number"
+            className="w-8 text-center border mx-2"
+            value={item.quantity}
+            onChange={() => {}}
+          />
+          <button className="bg-blue-500 text-white px-2 py-1" onClick={() => {}}>
+            +
+          </button>
         </div>
-    );
+      </td>
+      <td className="border px-4 py-2">${(item.price * item.quantity).toFixed(2)}</td>
+      <td className="border px-4 py-2">
+        <button className="text-red-500" onClick={() => {}}>
+        Delete
+        </button>
+      </td>
+
+
+
+
+    </tr>
+  );
 };
 
 export default CartItem;
--- shopping-cart-client/src/Pages/Cart/Cart.jsx
+++ shopping-cart-client/src/Pages/Cart/Cart.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
 import { useDispatch, useSelector } from "react-redux";
-import CartItem from "../../Components/CartItem/CartItem";
-
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
@@ -22,7 +20,41 @@ const Cart = () => {
         </thead>
         <tbody>
           {cart.map((item) => (
-            <CartItem key={item._id} item={item} />
+            // <CartItem key={item.id} item={item} />
+            <tr key={item._id}>
+              <td className="border px-4 py-2">{item.productName}</td>
+              <td className="border px-4 py-2">${item.price.toFixed(2)} u</td>
+              <td className="border px-4 py-2">
+                <div className="flex items-center">
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1"
+                    onClick={() => {}}
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    className="w-8 text-center border mx-2"
+                    value={item.quantity}
+                    onChange={() => {}}
+                  />
+                  <button
+                    className="bg-blue-500 text-white px-2 py-1"
+                    onClick={() => {}}
+                  >
+                    +
+                  </button>
+                </div>
+              </td>
+              <td className="border px-4 py-2">
+                ${(item.price * item.quantity).toFixed(2)}
+              </td>
+              <td className="border px-4 py-2">
+                <button className="text-red-500" onClick={() => {}}>
+                  Delete
+                </button>
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
